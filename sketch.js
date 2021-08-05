e381fef22d5b150bd716f3402bd436082a59e84f
80words <87866620+80words@users.noreply.github.com>
2021-08-04 21:18:28 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,54 @@
-var shipImg,ship ,edges;
-var seaImg;
+var pathImg,path
+var runnerImg, runner
+var boundary1, boundary2
+var edges
 
-var sea;
+
 
 
 function preload(){
-  seaImg=loadImage("sea.png")
+  //pre-load images
+pathImg=loadImage("path.png")
+runnerImg=loadAnimation("Runner-1.png","Runner-2.png")
 
-  shipImg=loadImage("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+
+
 
 }
 
 function setup(){
-  createCanvas(600,400);
-
-sea = createSprite(200,180,400,20)
-sea.addImage(seaImg)
-ship = createSprite(50,290,20,50);
-ship.addAnimation("running", shipImg);
-
-
-ship.scale = 0.25;
+  createCanvas(400,400);
+  //create sprites here
+createEdgeSprites
+boundary1=createSprite(0,200,40,400)
+boundary2=createSprite(400,200,40,400)
 
 
+path=createSprite(200,200)
+path.addImage(pathImg)
+path.velocityY=4
+path.scale=1.2
 
+runner=createSprite(300,200,10,10)
+runner.addAnimation("runner",runnerImg)
+runner.scale=0.08
 
 
 }
 
 function draw() {
-  //background("red");
-  sea.velocityX=-5
-  if (sea.x<0) {
-    sea.x=sea.width/2
-  }
-  drawSprites();
-//preload()
+  background(0);
+  path.velocityY=4
+if (path.y>400) {
+path.y=height/2
+}
+runner.x=World.mouseX
 
-//setup()
+runner.collide(boundary1)
+runner.collide(boundary2)
+
+boundary1.visible=false
+
+boundary2.visible=false
+drawSprites();
 }
